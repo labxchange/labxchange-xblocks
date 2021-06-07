b4700b0f71499b625ba34b7d397cb005b77afd91
toxinu <toxinu@gmail.com>
2021-06-07 10:55:22 +0900

    Change method name

--- labxchange_xblocks/annotated_video_block.py
+++ labxchange_xblocks/annotated_video_block.py
@@ -91,7 +91,7 @@ class AnnotatedVideoBlock(
     student_view_template = 'templates/annotated_video_student_view.html'
 
     @XBlock.handler
-    def student_view_user_state(self, request, suffix=""):
+    def student_view_data_and_user_state(self, request, suffix=""):
         """
         Return content and settings for student view.
         """
