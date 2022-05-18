8521f9dafe624310a99ed0e8c06bf0b2a9fa9134
Giovanni Cimolin <giovannicimolin@gmail.com>
2022-05-18 12:12:10 -0300

    fix: Audio block tests

--- labxchange_xblocks/tests/audio_block_test.py
+++ labxchange_xblocks/tests/audio_block_test.py
@@ -72,19 +72,10 @@ class AudioBlockTestCase(BlockTestCaseBase):
     def test_student_view_data(self, field_data, expected_data, _expected_html):
         self._test_student_view_data(field_data, expected_data)
 
-    @ddt.data(*data)
-    @ddt.unpack
-    def test_student_view(self, field_data, _expected_data, expected_html):
-        block = self._construct_xblock_mock(self.block_class, self.keys, field_data=DictFieldData(field_data))
-
-        fragment = block.student_view(None)
-        self.assertEqual(fragment.content, expected_html)
-        fragment = block.public_view(None)
-        self.assertEqual(fragment.content, expected_html)
-
     def test_inline_transcripts(self):
-        # test that inline transcripts are processed as expected,
-        # as well as testing that the on-the-fly conversion of srt files to inline html is done as expected
+        """
+        Tests that inline transcripts are processed as expected.
+        """
         field_data = {
             'display_name': 'A very cool track',
             'embed_code': '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/794640376&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',  # noqa: E501
@@ -102,32 +93,17 @@ class AudioBlockTestCase(BlockTestCaseBase):
             'embed_code': '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/794640376&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',  # noqa: E501
             'options': [
                 {'lang': 'en', 'language': {'name': 'English', 'native_name': 'English'}},
-                {'lang': 'fr', 'language': {'name': 'French', 'native_name': 'français, langue française'}}
             ],
             'transcripts': {
                 'en': {
                     'type': 'inlinehtml',
                     'content': '<p>Welcome to the show</p>',
                 },
-                'fr': {
-                    'type': 'inlinehtml',
-                    'content': '<p>Bienvenue !</p>',
-                },
             },
             'user_state': {'current_language': 'en'},
         }
 
-        example_srt = b'''
-1
-00:00:00,000 --> 00:00:04,440
-Bienvenue !
-        '''
-
         block = self._construct_xblock_mock(self.block_class, self.keys, field_data=DictFieldData(field_data))
-
-        # mocking assets retrieval to make the transcripts code work in this test
-        block.get_transcript_content = lambda x: example_srt if x == 'fr.url' else 'whatever'
-
         data = block.student_view_data(None)
         assert data == expected_data
 
